6c434090d0723b5f2f77d17ef93e94edb73c6c22
Madhu Kanth Kodali <122030756+madhukodali23@users.noreply.github.com>
2025-07-09 21:05:23 +0530

    Create BubbleSort.cpp

---
+++ BubbleSort.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+void bubbleSort(int arr[], int n) {
+    for(int i=n-1;i>=1;i--)
+    {
+         bool isSorted = true;
+        for(int j=0;j<=i-1;j++)
+        {
+            if (arr[j] > arr[j+1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                isSorted = false;
+            }
+        }
+        if (isSorted) break;
+    }
+}
+
+int main() {
+   int n;
+   cin >> n;
+   int arr[n];
+   for(int i=0;i<n;i++) {
+       cin >> arr[i];
+   }
+   
+   bubbleSort(arr, n);
+   
+   for(int i=0;i<n;i++) {
+       cout << arr[i] << " ";
+   }
+   return 0; 
+}
