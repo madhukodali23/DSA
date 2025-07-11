000e5a6989747e36a498ae2d9b1a53a1056ce547
Madhu Kanth Kodali <122030756+madhukodali23@users.noreply.github.com>
2025-07-11 06:05:36 +0530

    Create InsertionSortCountingNumberOfSwaps.cpp

---
+++ InsertionSortCountingNumberOfSwaps.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+void insertionSort(int n, int arr[]) 
+{
+    #include <bits/stdc++.h>
+using namespace std;
+
+class solution {
+public:
+    int countSwaps(int arr[], int n){
+        int count = 0;
+        for(int i=1;i<=n-1;i++)
+        {
+            int j = i;
+            while (arr[j-1] > arr[j] && j >= 1)
+            {
+                int temp = arr[j-1];
+                arr[j-1] = arr[j];
+                arr[j] = temp;
+                count++;
+                j--;
+            }
+        }
+        return count;
+    }
+};
+}
+ 
+int main() {
+  int n;
+  cin >> n;
+  int arr[n];
+  for(int i=0;i<n;i++)
+  {
+      cin >> arr[i];
+  }
+  insertionSort( n, arr);
+  for(int i=0;i<n;i++) {
+    cout << arr[i] << " ";   
+  }
+  
+  return 0;
+}
