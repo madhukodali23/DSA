078b62754508ca59ade3e69839f811515a2b6687
Madhu Kanth Kodali <122030756+madhukodali23@users.noreply.github.com>
2025-07-24 21:55:53 +0530

    Create MoveNPositionsBruteForce.cpp

---
+++ MoveNPositionsBruteForce.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void move_n_positions_left(int arr[], int n, int k)
+{
+    vector<int> v;
+    int j = 0;
+    for(int i=0;i<=k-1;i++) v.push_back(arr[i]);
+    for(int i=k;i<=n-1;i++) {
+        arr[j] = arr[i];
+        j++;
+    }
+    for(auto k : v) {
+        arr[j] = k;
+        j++;
+    }
+}
+ 
+int main() {
+  int n, k;
+  cin >> n >> k;
+  int arr[n];
+  for(int i=0;i<=n-1;i++) cin >> arr[i];
+  move_n_positions_left(arr, n, k);
+  for(int i=0;i<=n-1;i++) cout << arr[i] << " ";
+  return 0;
+}
