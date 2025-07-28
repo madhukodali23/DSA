d3c5b821a31bbdb8a9fdb660f4e3c8ec4911db69
Madhu Kanth Kodali <122030756+madhukodali23@users.noreply.github.com>
2025-07-28 22:30:23 +0530

    Update CountConsecutiveOnes.cpp

--- CountConsecutiveOnes.cpp
+++ CountConsecutiveOnes.cpp
@@ -1,26 +1,20 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int countOnes(int arr[], int n) {
-    int count = 0;
-    for(int i=0;i<n;i++) {
-        if (arr[i] == 1) {
-            count++;
-        }else {
-            count = 0;
+class solution{
+public:
+    int maxConsecutiveOnes(int arr[], int n){
+        int count = 0;
+        int ans = 0;
+        for(int i=0;i<n;i++) {
+            if (arr[i] == 1) {
+                count++;
+                ans = max(ans, count);
+            }
+            else {
+                count = 0;
+            }
         }
+        return ans;
     }
-    return count;
-}
- 
-int main() {
-  int n;
-  cin >> n;
-  int arr[n];
-  for(int i=0;i<=n-1;i++) {
-      cin >> arr[i];
-  }
-  int result = countOnes(arr, n);
-  cout << result << endl;
-  return 0;
-}
+};
