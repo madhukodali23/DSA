635c5cca095c869dff6de3415d864f8502f558b2
Madhu Kanth Kodali <122030756+madhukodali23@users.noreply.github.com>
2025-08-10 15:41:29 +0530

    Create BuiltInLowerBound.cpp

---
+++ BuiltInLowerBound.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// int lowerBoundBS(int arr[], int n, int target)
+// {
+//     int l = 0;
+//     int r = n-1;
+//     int ans = n;
+//     while (l <= r)
+//     {
+//         int mid = ( l + r ) / 2;
+//         if (arr[mid] >= target) 
+//         {
+//             r = mid - 1;
+//             ans = mid;
+//         }
+//         else 
+//         {
+//             l = mid + 1;
+//         }
+//     }
+//     return l;
+// }
+
+int main() {
+  int n, target;
+  cin >> n >> target;
+  int arr[n];
+  for(int i=0;i<=n-1;i++) cin >> arr[i];
+  cout <<  lower_bound(arr, arr + n, target) - arr ;
+  return 0;
+}
