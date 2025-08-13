4875103f2ee5f438fbc69b45de906c00cb37b2a8
Madhu Kanth Kodali <122030756+madhukodali23@users.noreply.github.com>
2025-08-13 06:51:25 +0530

    Create lastOccuranceofNumber.cpp

---
+++ lastOccuranceofNumber.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int lastOccuranceOfANumber(int arr[], int n, int target)
+{
+    int low = 0;
+    int high = n-1;
+    int ans = -1;
+    while (low <= high)
+    {
+        int mid = (low + high)/2;
+        if (arr[mid] <= target) {
+            low = mid + 1;
+            ans = mid;
+        }
+        else if (arr[mid] > target)
+        {
+            high = mid - 1;
+        }
+    }
+    return ans;
+}
+ 
+int main() {
+  int n, target;
+  cin >> n >> target;
+  int arr[n];
+  for(int i=0;i<=n-1;i++) cin >> arr[i];
+  cout << lastOccuranceOfANumber(arr, n, target);
+  return 0;
+}
