851abb971189d4ade5cf269a004f95d335c782b5
Madhu Kanth Kodali <122030756+madhukodali23@users.noreply.github.com>
2025-08-19 21:35:24 +0530

    Create squareRootOfNLinear.cpp

---
+++ squareRootOfNLinear.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int n;
+  cin >> n;
+  int ans = -1;
+  for(int i=1;i<=n;i++)
+  {
+      if (i * i <= n)
+      {
+          ans = i;
+      }
+      else
+      {
+          break;
+      }
+  }
+  cout << ans << endl;
+  return 0;
+}
