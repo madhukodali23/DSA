95c43db83f40a431f3f3d61464052fe730fc195f
Madhu Kanth Kodali <122030756+madhukodali23@users.noreply.github.com>
2025-09-06 15:53:22 +0530

    Create minimumPossibleDivisorBruteForce.cpp

---
+++ minimumPossibleDivisorBruteForce.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPossible(int arr[], int n, int limit, int m)
+{
+    int sum = limit;
+    for(int i=0;i<=n-1;i++)
+    {
+        sum -= arr[i] / m;
+        if (arr[i] % m != 0) 
+        {
+            sum--;
+        }
+        if (sum < 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int findSmallestDivisor(int arr[], int n, int limit)
+{
+  int low = 0;
+  int high = *max_element(arr, arr+n);
+  int ans = high;
+  while(low <= high)
+    {
+      int mid = (low + high)/2;
+      if (isPossible(arr, n, limit,  mid))
+      {
+          ans = mid;
+          high = mid - 1;
+      }
+      else
+      {
+          low = mid + 1;
+      }
+    }
+    return ans;
+}
+ 
+int main() {
+  int arr[] = {9, 35, 19, 17, 7};
+  int n = 5;
+  int limit = 11;
+  cout << findSmallestDivisor(arr, n, limit);
+  return 0;
+}
