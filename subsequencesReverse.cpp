4cf947e4baed7c460e6e23ad9bd576ebbcfa239f
Madhu Kanth Kodali <122030756+madhukodali23@users.noreply.github.com>
2025-09-13 08:32:24 +0530

    Create subsequencesReverse.cpp

---
+++ subsequencesReverse.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void subsequences(int arr[], int n, vector<int>& v, int i)
+{
+    if (i == n) {
+        for(auto k : v) {
+            cout << k << " ";
+        }
+        cout << endl;
+        return;
+    }
+    subsequences(arr, n, v, i+1);
+    v.push_back(arr[i]);
+    subsequences(arr, n, v, i+1);
+    v.pop_back();
+}
+ 
+int main() {
+  int n = 3, i = 0;
+  int arr[] = {4, 5, 6};
+  vector<int> v;
+  subsequences(arr, n, v, i);
+  return 0;
+}
