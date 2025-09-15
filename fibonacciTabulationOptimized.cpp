01d1d3c18ca41071bb06078fc3f01044eca177d4
Madhu Kanth Kodali <122030756+madhukodali23@users.noreply.github.com>
2025-09-15 21:03:28 +0530

    Create fibonacciTabulationOptimized.cpp

---
+++ fibonacciTabulationOptimized.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int fibonacci(int n)
+{
+    int priori_1 = 0;
+    int priori_2 = 1;
+    int current;
+    for(int i=2;i<=n;i++)
+    {
+        current = priori_1 + priori_2;
+        priori_1 = priori_2;
+        priori_2 = current;
+    }
+    return current;
+}
+ 
+int main() {
+  int n;
+  cin >> n;
+  cout << fibonacci(n);
+  return 0;
+}
