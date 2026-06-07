e6af476eccd05e80f5fc816c260227c8c074fb23
Madhu Kanth Kodali <122030756+madhukodali23@users.noreply.github.com>
2026-06-07 11:54:46 +0530

    Create arrangeColorsInOrder.cpp

---
+++ arrangeColorsInOrder.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class solution {
+public:
+    void arrangeColorsInOrder(vector<int>& arr) {
+        int n = arr.size();
+        int low = 0, mid = 0, high = n-1;
+        while (mid <= high)
+        {
+            if (arr[mid] == 0)
+            {
+                swap(arr[mid], arr[low]);
+                mid++;
+                low++;
+            }
+            else if (arr[mid] == 1)
+            {
+                mid++;
+            }
+            else 
+            {
+                swap(arr[mid], arr[high]);
+                high--;
+            }
+        }
+    }
+};
